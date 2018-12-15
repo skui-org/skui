9a5fbcb0aba29aa72209402b4cbc2fb23955069e
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2018-12-15 17:56:07 +0100

    Use CMake from Github.

--- ci/setup_cmake.sh
+++ ci/setup_cmake.sh
@@ -7,6 +7,6 @@ MAJOR=`echo ${VERSION} | cut -d. -f1`
 MINOR=`echo ${VERSION} | cut -d. -f2`
 REVISION=`echo ${VERSION} | cut -d. -f3`
 
-wget https://cmake.org/files/v${MAJOR}.${MINOR}/cmake-${VERSION}-${OS}-x86_64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v${MAJOR}.${MINOR}.${REVISION}/cmake-${VERSION}-${OS}-x86_64.tar.gz
 tar -xf cmake-${VERSION}-${OS}-x86_64.tar.gz
 export PATH=${PWD}/cmake-${VERSION}-${OS}-x86_64/bin:${PATH}
