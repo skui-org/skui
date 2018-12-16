2fb390cb6bd059aae5bf64c3adf2b419028b491d
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2018-12-16 10:10:18 +0100

    Add message after cmake setup CI script.

--- ci/setup_cmake.sh
+++ ci/setup_cmake.sh
@@ -10,3 +10,5 @@ REVISION=`echo ${VERSION} | cut -d. -f3`
 wget https://github.com/Kitware/CMake/releases/download/v${MAJOR}.${MINOR}.${REVISION}/cmake-${VERSION}-${OS}-x86_64.tar.gz
 tar -xf cmake-${VERSION}-${OS}-x86_64.tar.gz
 export PATH=${PWD}/cmake-${VERSION}-${OS}-x86_64/bin:${PATH}
+echo "Extracted CMake and set PATH=$PATH"
+cmake --version
