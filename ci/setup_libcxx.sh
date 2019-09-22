b5d1bc668b733b86d4b47da1d308ac8040847773
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2019-09-22 11:26:42 +0200

    Upgrade libc++ versions on Travis CI builds. Handle release location disparity for LLVM tarballs.

--- ci/setup_libcxx.sh
+++ ci/setup_libcxx.sh
@@ -7,9 +7,18 @@ MAJOR=`echo ${VERSION} | cut -d. -f1`
 MINOR=`echo ${VERSION} | cut -d. -f2`
 REVISION=`echo ${VERSION} | cut -d. -f3`
 
-wget http://llvm.org/releases/${VERSION}/llvm-${VERSION}.src.tar.xz
-wget http://llvm.org/releases/${VERSION}/libcxxabi-${VERSION}.src.tar.xz
-wget http://llvm.org/releases/${VERSION}/libcxx-${VERSION}.src.tar.xz
+case ${VERSION} in
+  7.1.0|8.0.1)
+    URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-
+    ;;
+  *)
+    URL=http://releases.llvm.org/
+    ;;
+esac
+
+wget ${URL}${VERSION}/llvm-${VERSION}.src.tar.xz
+wget ${URL}${VERSION}/libcxxabi-${VERSION}.src.tar.xz
+wget ${URL}${VERSION}/libcxx-${VERSION}.src.tar.xz
 mkdir -p llvm/projects/libcxxabi llvm/projects/libcxx
 tar --strip-components=1 -xf llvm-${VERSION}.src.tar.xz -C llvm
 tar --strip-components=1 -xf libcxxabi-${VERSION}.src.tar.xz -C llvm/projects/libcxxabi
